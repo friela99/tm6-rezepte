29ef1e3ac284eb35c4287d508dd0924fc095dd5c
friela99 <friela@mail.de>
2026-08-18 13:19:20 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
-const q=document.getElementById('q');q.oninput=()=>{document.querySelectorAll('.card').forEach(c=>c.style.display=c.innerText.toLowerCase().includes(q.value.toLowerCase())?'block':'none')}
+const s=document.getElementById('search');
+s.oninput=()=>document.querySelectorAll('.card').forEach(c=>{
+c.style.display=c.innerText.toLowerCase().includes(s.value.toLowerCase())?'block':'none';
+});
